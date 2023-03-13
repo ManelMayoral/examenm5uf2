a79b148d156aa21c14e14aa4430ddb22db8a109f
ManelMayoral <manelmayoralcano7@gmail.com>
2023-03-13 08:41:28 +0100

    Nova versio del projecte de c# i documentacio ex5

--- examenGIT/examenGIT/Frm_main.cs
+++ examenGIT/examenGIT/Frm_main.cs
@@ -12,9 +12,21 @@ namespace examenGIT
 {
     public partial class Frm_main : Form
     {
+        frm_menjar menjar = new frm_menjar();
+        frm_sitio sitio = new frm_sitio();  
         public Frm_main()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            menjar.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sitio.Show();
         }
     }
 }
